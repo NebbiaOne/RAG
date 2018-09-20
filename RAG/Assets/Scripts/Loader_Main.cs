21a0cbc9b219b1cc0d34a45978a1ade92b476db1
Ruben <38580042+Vandermerwer@users.noreply.github.com>
2018-09-20 23:31:41 +0200

    Basic Menu navigation
    - Basic Menu Navigattion.
    - Basic Overlord

---
+++ RAG/Assets/Scripts/Loader_Main.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Loader_Main : MonoBehaviour {
+	public static Loader_Main _Loader_Main;
+	Overlord_Main _Overlord;
+	AsyncOperation asyncLoad;
+	string nextScene;
+	bool enumerating = false;
+	void Awake () {
+		_Loader_Main = this;
+	}
+	void Start () {
+		_Overlord = Overlord_Main._Overlord_main;
+		nextScene = "MainMenu";
+	}
+	void Update () {
+		if (SceneManager.GetActiveScene ().name == "Loading" && enumerating == false && nextScene != null) {
+			StartCoroutine (LoadLevel());
+			enumerating = true;
+		}
+	}
+	IEnumerator LoadLevel () {
+		asyncLoad = SceneManager.LoadSceneAsync(nextScene);
+		while (!asyncLoad.isDone) {
+			if (asyncLoad.allowSceneActivation != false) {
+				asyncLoad.allowSceneActivation = false;
+			}
+			if (asyncLoad.progress >= 0.9f) {
+				yield return new WaitForSeconds (1f);
+				asyncLoad.allowSceneActivation = true;
+				enumerating = false;
+				nextScene = null;
+			}
+			yield return null;
+		}
+	}
+	public void Load_MainMenu () {
+		nextScene = "MainMenu";
+		SceneManager.LoadScene ("Loading");
+	}
+	public void Load_TempArena () {
+		nextScene = "testArena";
+		SceneManager.LoadScene ("Loading");
+	}
+	public void Load_Quit () {
+		Application.Quit ();
+	}
+}
---
+++ RAG/Assets/Scripts/Overlord_Main.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Overlord_Main : MonoBehaviour {
+	public static Overlord_Main _Overlord_main;
+	bool pauseChecked = true, isArena = false;
+	public GameObject pauseMenu;
+	[HideInInspector]
+	public bool paused = false;
+	void Awake () {
+		if (this.gameObject != null) {
+			DontDestroyOnLoad (this.gameObject);
+			_Overlord_main = this;
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+	void Start () {
+		if (SceneManager.GetActiveScene ().name == "testArena") {
+			isArena = true;
+		} else {
+			isArena = false;
+		}
+	}
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (paused == false && pauseChecked == true && isArena == true) {
+				Debug.Log ("Paused");
+				paused = true;
+				pauseChecked = false;
+				pauseMenu.SetActive (true);
+				Invoke ("PauseChecker", 0.25f);
+			}
+			if (paused == true && pauseChecked == true && isArena == true) {
+				Debug.Log ("Not Paused");
+				paused = false;
+				pauseChecked = false;
+				pauseMenu.SetActive (false);
+				Invoke ("PauseChecker", 0.25f);
+			}
+		}
+	}
+	void PauseChecker () {
+		pauseChecked = true;
+	}
+}
