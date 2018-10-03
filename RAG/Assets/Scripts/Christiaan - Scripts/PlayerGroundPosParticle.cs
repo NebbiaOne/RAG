88070ddd92bf3d08a89e27d27ec81b86bb14091d
SpaceDassie <cjdjcj@gmail.com>
2018-10-03 15:31:29 +0200

    Startup effect and ground point particles
    Added a nice way for the platforms to rise, and made a particle system that shows where the player is above the ground to make it easy to position self when in air

---
+++ RAG/Assets/Scripts/Christiaan - Scripts/PlayerGroundPosParticle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundPosParticle : MonoBehaviour {
+
+	public GameObject GroundPosParticle;
+
+	public Vector3 GroundPosHitPoint;
+	public GameObject RayOrigin;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit GroundPosHitPoint;
+		if (Physics.Raycast(RayOrigin.transform.position,(RayOrigin.transform.up * -1),out GroundPosHitPoint,100))
+		{
+
+			GroundPosParticle.transform.position= GroundPosHitPoint.point;
+			Debug.Log(GroundPosHitPoint.point);
+		}
+		
+	}
+}
