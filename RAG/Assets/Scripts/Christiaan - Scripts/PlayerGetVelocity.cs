32e318aa3fd39a8c050b128a628e55193ac754ce
SpaceDassie <cjdjcj@gmail.com>
2018-10-26 01:42:27 +0200

    animator and script
    added some more animator functionality and script

---
+++ RAG/Assets/Scripts/Christiaan - Scripts/PlayerGetVelocity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGetVelocity : MonoBehaviour {
+	private Rigidbody rd;
+	private float velocity;
+	public Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		rd = GetComponent<Rigidbody>();
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity = rd.velocity.magnitude;
+		
+		if (velocity > 0.5f){
+			anim.SetBool("Moving", true);
+		} else anim.SetBool("Moving", false);
+	}
+}
