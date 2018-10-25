3008b6fffaf316e08978743fb4e44e68c221b684
SpaceDassie <cjdjcj@gmail.com>
2018-10-25 23:26:57 +0200

    particles and scripts
    added blood particles and some scripts for platforms

---
+++ RAG/Assets/Scripts/Christiaan - Scripts/SetPlatformHeight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPlatformHeight : MonoBehaviour {
+
+private int Position = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(SetPosition());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator SetPosition()
+	{
+        yield return new WaitForSeconds(0.5f);
+        Position = Random.Range(0, 3);
+       //Debug.Log(Position);
+        transform.Translate(0, Position, 0, Space.Self);
+		//MoveNow();
+        
+	}
+
+	// void MoveNow()
+	// {
+	// 	transform.Translate(0,Position*Time.deltaTime,0,Space.Self);	
+	// }
+}
