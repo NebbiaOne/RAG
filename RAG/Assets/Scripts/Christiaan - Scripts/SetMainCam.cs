2b6a605bbe34836bcd2ad15fdd3d0c5c1461ef6b
SpaceDassie <cjdjcj@gmail.com>
2018-11-09 04:28:07 +0200

    move
    YOU CAN MOVE AGAIN

---
+++ RAG/Assets/Scripts/Christiaan - Scripts/SetMainCam.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMainCam : MonoBehaviour {
+
+public Camera MainCam;
+	// Use this for initialization
+	void Start () {
+		MainCam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
