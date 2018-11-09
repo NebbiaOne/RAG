db344f849d9edcb0cbf833a864e23ddda03a4d04
SpaceDassie <cjdjcj@gmail.com>
2018-11-09 02:48:28 +0200

    death particle
    particles now spawn at death location

--- RAG/Assets/Scripts/Christiaan - Scripts/TestLevelRespawn.cs
+++ RAG/Assets/Scripts/Christiaan - Scripts/TestLevelRespawn.cs
@@ -6,7 +6,12 @@ public class TestLevelRespawn : MonoBehaviour {
 	public GameObject CurrentPlayer;
 	public GameObject SpawnPoint;
 	public GameObject WaterCollider;
+	public GameObject SkullParticle;
+	private Vector3 DeathParticleSpawnPos;
+	GameObject TempObj;
+	ContactPoint ContactP;
  	private void OnCollisionEnter(Collision collision) 
+	 
 	 {
 		if (collision.gameObject.tag == "Player_01" || collision.gameObject.tag == "Player_02" || collision.gameObject.tag == "Player_03" || collision.gameObject.tag == "Player_04")
 		{
@@ -15,6 +20,14 @@ public class TestLevelRespawn : MonoBehaviour {
 			_PlayerMain.playerLives -= 1;
 			//CurrentPlayer.transform.position = WaterCollider.transform.position;
 			CurrentPlayer.transform.position = new Vector3(0.0f, 20.0f,0.0f);
+            //ContactPoint.point = DeathParticleSpawnPos;
+            ContactP = collision.contacts[0];
+			DeathParticleSpawnPos = ContactP.point;
+            Debug.Log(DeathParticleSpawnPos);
+            TempObj = (GameObject)Instantiate(SkullParticle);
+            TempObj.transform.position = DeathParticleSpawnPos;
+           
+
 		}	 
 	 }
 }
